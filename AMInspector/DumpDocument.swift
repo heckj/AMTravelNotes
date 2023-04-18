0a78a2fdf9b88810073a311ad0ab98ba695d4839
Joe Heck <heckj@mac.com>
2023-04-18 09:13:10 -0700

    handling more error scenarios gracefully

--- AMInspector/DumpDocument.swift
+++ AMInspector/DumpDocument.swift
@@ -6,15 +6,30 @@ import Foundation
 struct AMInspector: ParsableCommand {
     @Argument(help: "The Automerge document to inspect.")
     var inputFile: String
-    
-    @Flag(name: [.customShort("l"), .long],
-          help: "List out the included changesets.")
+
+    @Flag(
+        name: [.customShort("l"), .long],
+        help: "List out the included changesets."
+    )
     var listchanges = false
 
     mutating func run() throws {
-        let data = try Data(contentsOf: URL(fileURLWithPath: inputFile))
-                
-        let doc = try Document(data)
+        let data: Data
+        let doc: Document
+        do {
+            data = try Data(contentsOf: URL(fileURLWithPath: inputFile))
+        } catch {
+            print("Unable to open file at \(inputFile).")
+            AMInspector.exit(withError: error)
+        }
+
+        do {
+            doc = try Document(data)
+        } catch {
+            print("\(inputFile) is not an Automerge document.")
+            AMInspector.exit(withError: error)
+        }
+
         let changesets = doc.heads()
         print("Filename: \(inputFile)")
         print("- Size: \(data.count) bytes")
