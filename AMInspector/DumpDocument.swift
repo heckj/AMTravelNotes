25611c151edd3f3dd25cbcd6ffb3c87c7ec44ebe
Joe Heck <heckj@mac.com>
2023-04-18 11:30:01 -0700

    CLI to dump schema and data about document

--- AMInspector/DumpDocument.swift
+++ AMInspector/DumpDocument.swift
@@ -9,9 +9,12 @@ struct AMInspector: ParsableCommand {
 
     @Flag(
         name: [.customShort("l"), .long],
-        help: "List out the included changesets."
+        help: "List out the changeset hashes in the summary."
     )
     var listchanges = false
+
+    @Flag(help: "Walk the schema of the document and print out the resolved values.")
+    var walk = false
 
     mutating func run() throws {
         let data: Data
@@ -39,6 +42,54 @@ struct AMInspector: ParsableCommand {
             for cs in changesets {
                 print("  - \(cs)")
             }
+        }
+
+        if walk {
+            do {
+                print("SCHEMA AND DATA:")
+                print("{")
+                try walk(doc)
+                print("}")
+            } catch {
+                print("Error while walking document.")
+                AMInspector.exit(withError: error)
+            }
+        }
+    }
+
+    func walk(_ doc: Document, from objId: ObjId = ObjId.ROOT, indent: Int = 1) throws {
+        let indentString = String(repeating: " ", count: indent * 2)
+        switch doc.objectType(obj: objId) {
+        case .Map:
+            for (key, value) in try doc.mapEntries(obj: objId) {
+                if case let Value.Scalar(scalarValue) = value {
+                    print("\(indentString)\"\(key)\" : \(scalarValue)")
+                }
+                if case let Value.Object(childObjId, _) = value {
+                    print("\(indentString)\"\(key)\" : {")
+                    try walk(doc, from: childObjId, indent: indent + 1)
+                    print("\(indentString)}")
+                }
+            }
+        case .List:
+            if doc.length(obj: objId) == 0 {
+                print("\(indentString)[]")
+            } else {
+                print("\(indentString)[")
+                for value in try doc.values(obj: objId) {
+                    if case let Value.Scalar(scalarValue) = value {
+                        print("\(indentString)  \(scalarValue)")
+                    } else {
+                        if case let Value.Object(childObjId, _) = value {
+                            try walk(doc, from: childObjId, indent: indent + 1)
+                        }
+                    }
+                }
+                print("\(indentString)]")
+            }
+        case .Text:
+            let stringValue = try doc.text(obj: objId)
+            print("\(indentString)Text[\"\(stringValue)\"]")
         }
     }
 }
