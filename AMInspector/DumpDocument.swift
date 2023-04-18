9c938395fe37d4b6c7b8632e471ace213eb6d6d2
Joe Heck <heckj@mac.com>
2023-04-18 08:22:20 -0700

    walking doc

--- AMInspector/DumpDocument.swift
+++ AMInspector/DumpDocument.swift
@@ -3,16 +3,27 @@ import Automerge
 import Foundation
 
 @main
-struct DumpDocument: ParsableCommand {
-    @Argument var inputFile: String
-    @Argument var outputFile: String
+struct AMInspector: ParsableCommand {
+    @Argument(help: "The Automerge document to inspect.")
+    var inputFile: String
+    
+    @Flag(name: [.customShort("l"), .long],
+          help: "List out the included changesets.")
+    var listchanges = false
 
     mutating func run() throws {
-        print("""
-        Counting words in '\(inputFile)' \
-        and writing the result into '\(outputFile)'.
-        """)
-
-        // Read 'inputFile', count the words, and save to 'outputFile'.
+        let data = try Data(contentsOf: URL(fileURLWithPath: inputFile))
+                
+        let doc = try Document(data)
+        let changesets = doc.heads()
+        print("Filename: \(inputFile)")
+        print("- Size: \(data.count) bytes")
+        print("- ActorId: \(doc.actor)")
+        print("- ChangeSets: \(doc.heads().count)")
+        if listchanges {
+            for cs in changesets {
+                print("  - \(cs)")
+            }
+        }
     }
 }
