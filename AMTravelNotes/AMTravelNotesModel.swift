4aab8851bfdc2d50d461c5d71190dc1459ad2a59
Joe Heck <heckj@mac.com>
2023-04-18 12:52:12 -0700

    exploring AutomergeRepresentable - but I think I need to jump entirely to a Coder

--- AMTravelNotes/AMTravelNotesModel.swift
+++ AMTravelNotes/AMTravelNotesModel.swift
@@ -12,6 +12,8 @@ class TravelNotesModel: BaseAutomergeBoundObject, Identifiable {
     @AmText("notes") var notes: String
     @AmObj("meta") var subObject: BaseAutomergeBoundObject
 
+    // @AmStruct("meta2") var subObject2: CoupleOfThings
+
     var computedProperty: Bool {
         done
     }
@@ -58,5 +60,48 @@ struct CoupleOfThings: Identifiable {
         doubleExample = Double.pi
         listExample = [5]
         dictExample = ["one": 1]
+    }
+}
+
+// alternately, I might be able to double-up on Codable representations
+// to come up with an appropriate representation.
+// struct -> Encodable -> JSON -> convert to Automerge types?
+
+extension CoupleOfThings: AutomergeRepresentable {
+    public enum MyStructConversionError: LocalizedError {
+        case notMap(_ val: Value)
+        case unimplemented
+
+        /// A localized message describing what error occurred.
+        public var errorDescription: String? {
+            switch self {
+            case let .notMap(val):
+                return "The value \(val) is not a Map object and can't be bound to the struct."
+            case .unimplemented:
+                return "This feature isn't fully implemented."
+            }
+        }
+    }
+
+    static func fromValue(_ val: Automerge.Value) throws -> CoupleOfThings {
+        guard case let .Object(mapObjectId, .Map) = val else {
+            throw MyStructConversionError.notMap(val)
+        }
+        print(mapObjectId)
+        // do the work to inspect the relevant type and bind in the keys
+        // for the struct, recursively doing "the right thing", returning
+        // a populated Struct "CoupleOfThings" from the underlying Automerge
+        // pieces.
+        throw MyStructConversionError.unimplemented
+    }
+
+    // extend to support dropping into a List and/or Map with an additional
+    // option of "key" for Map and indexPosition for List?
+    func toValue(doc _: Automerge.Document, objId _: Automerge.ObjId) throws -> Automerge.Value {
+        // takes an instance of the struct and encodes it's various tidbits
+        // into Automerge, using the provided objId (which should be a Map)
+        // as the enclosing object instance
+
+        Value.Scalar(.Null)
     }
 }
