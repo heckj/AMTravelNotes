c828c8e60ccff73812fab3d82abd6daf729eb874
Joe Heck <heckj@mac.com>
2023-05-16 15:29:26 -0700

    estabishing test plans

--- AMTravelNotesTests/AutomergeEncoderImpl+RetrieveTests.swift
+++ AMTravelNotesTests/AutomergeEncoderImpl+RetrieveTests.swift
@@ -11,19 +11,45 @@ import XCTest
 
 final class RetrieveObjectIdTests: XCTestCase {
     var doc: Document!
+    var setupCache: [String: ObjId] = [:]
 
     override func setUp() {
+        setupCache = [:]
         doc = Document()
         let list = try! doc.putObject(obj: ObjId.ROOT, key: "list", ty: .List)
+        setupCache["list"] = list
+
         let nestedMap = try! doc.insertObject(obj: list, index: 0, ty: .Map)
+        setupCache["nestedMap"] = nestedMap
+
         try! doc.put(obj: nestedMap, key: "image", value: .Bytes(Data()))
-        let _ = try! doc.putObject(obj: nestedMap, key: "notes", ty: .Text)
+        let deeplyNestedText = try! doc.putObject(obj: nestedMap, key: "notes", ty: .Text)
+        setupCache["deeplyNestedText"] = deeplyNestedText
     }
 
     func testPathAtRoot() throws {
         let doc = Document()
         let path = try! doc.path(obj: ObjId.ROOT)
-        print("\(path)")
         XCTAssertEqual(path, [])
+    }
+
+    func testRetrieveLeafValue() throws {
+        let fullCodingPath: [AnyCodingKey] = [
+            AnyCodingKey("list"),
+            AnyCodingKey(0),
+            AnyCodingKey("notes"),
+        ]
+        let encoderImpl = AutomergeEncoderImpl(userInfo: [:], codingPath: fullCodingPath, doc: doc)
+
+        let result = encoderImpl.retrieveObjectId(path: fullCodingPath, containerType: .Value)
+        switch result {
+        case let .success((objectId, codingKeyInstance)):
+            XCTAssertEqual(objectId, setupCache["nestedMap"])
+            XCTAssertEqual(codingKeyInstance, AnyCodingKey("notes"))
+        case .failure:
+            XCTFail("Failure looking up full path to notes as a value")
+        }
+        // Caching not yet implemented
+        // XCTAssertEqual(encoderImpl.cache.count, 2)
     }
 }
